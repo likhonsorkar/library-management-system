f55c4b6afec12b03bbe19f6984ee5116dd96e2d9
unknown <likhonsorkar002@gmail.com>
2025-12-07 10:13:42 +0600

    All Model implement and Register Admin

--- books/models.py
+++ books/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    bio = models.TextField(blank=True, null=True)
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author,on_delete=models.CASCADE, related_name="books")
+    ISBN = models.CharField(max_length=17, blank=True, null= True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="books")
+    stock = models.PositiveIntegerField()
+    def __str__(self):
+        return self.title
--- borrow/admin.py
+++ borrow/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from borrow.models import BorrowRecord
 
-# Register your models here.
+@admin.register(BorrowRecord)
+class BorrowRecordAdmin(admin.ModelAdmin):
+    list_display = ('book', 'member', 'borrow_date', 'due_date', 'return_date')
+    list_filter = ('borrow_date', 'return_date')
+    search_fields = ('book__title', 'member__email')
+    readonly_fields = ('borrow_date',)
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'member')
+        }),
+        ('Dates', {
+            'fields': ('borrow_date', 'due_date', 'return_date')
+        }),
+    )
