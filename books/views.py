9bf12a1fea4180f181d079577b5ae0d0eb2ab384
unknown <likhonsorkar002@gmail.com>
2025-12-23 11:41:33 +0600

    Permission

--- books/views.py
+++ books/views.py
@@ -1,15 +1,19 @@
 from rest_framework.viewsets import ModelViewSet
 from books.models import Book, Category, Author
 from books.serializers import BookSerializers, CategorySerializers, AuthorSerializers
+from books.permissions import IsLibrarianOrReadOnly
 # Create your views here.
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializers
+    permission_classes = [IsLibrarianOrReadOnly]
 class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializers
+    permission_classes = [IsLibrarianOrReadOnly]
 class AuthorViewSet(ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializers
+    permission_classes = [IsLibrarianOrReadOnly]
 
     
--- borrow/views.py
+++ borrow/views.py
@@ -1,10 +1,17 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from borrow.models import BorrowRecord
 from borrow.serializers import BorrowRecordSerializers
 
 class BorrowRecordViewSet(ModelViewSet):
     serializer_class = BorrowRecordSerializers
+    permission_classes = [IsAuthenticated]
     def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            queryset = BorrowRecord.objects.all()
+        else:
+            queryset = BorrowRecord.objects.filter(member=user)
         if self.request.query_params.get('active') == 'true':
-            return BorrowRecord.objects.filter(return_date__isnull=True)
-        return BorrowRecord.objects.all()
+            return queryset.filter(return_date__isnull=True)
+        return queryset
