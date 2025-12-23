4ba127db28af33ecba5a13fff6d27711b07c1ea0
unknown <likhonsorkar002@gmail.com>
2025-12-23 10:43:04 +0600

    DJoser + jwt

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, include
 from books.views import BookViewSet, CategoryViewSet, AuthorViewSet
-from members.views import MemberViewSet
 from borrow.views import BorrowRecordViewSet
 from rest_framework import routers
 
@@ -8,7 +7,6 @@ router = routers.DefaultRouter()
 router.register('books', BookViewSet, basename="books")
 router.register('categories', CategoryViewSet, basename="categories")
 router.register('authors', AuthorViewSet, basename="authors")
-router.register('members', MemberViewSet, basename='members')
 router.register('borrow-records', BorrowRecordViewSet, basename='borrow-records')
 urlpatterns = [
     path('', include(router.urls))
