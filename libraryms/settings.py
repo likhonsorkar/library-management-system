ee226535f6cb6ca5d5077e6b113a1279ee3a4b2b
unknown <likhonsorkar002@gmail.com>
2025-12-29 18:33:37 +0600

    Swagger

--- libraryms/settings.py
+++ libraryms/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'djoser',
     'django_filters',
+    'drf_yasg',
     'api',
     'books',
     'borrow',
--- libraryms/urls.py
+++ libraryms/urls.py
@@ -16,11 +16,29 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Library Management System API",
+      default_version='v1',
+      description="This RESTFull API Library management system, Librarian can Create, Update, Read and Delete Book and author and can update Return book, Member can borrow book",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="developer@likhon.com.bd"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("api.urls")),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
